ff66d92675177d753def14384e07d4eb799c0cf6
oznogon <oznogon@users.noreply.github.com>
2026-07-05 15:56:17 -0700

    [fork] Reformat entity.h

--- src/ecs/entity.h
+++ src/ecs/entity.h
@@ -6,105 +6,117 @@
 
 #include "component.h"
 
-
 class MultiplayerObject;
 class GameServer;
 class GameClient;
-namespace sp::ecs {
 
+namespace sp::ecs
+{
 // An entity is a lightweight, copyable reference to entity components.
-//	Entities have to be created with the create() function, and explicitly destroy()ed.
-class Entity final {
+// Entities have to be created with the create() function, and explicitly destroy()ed.
+class Entity final
+{
 public:
-	Entity() = default;
-	
-	static Entity create();
-	void destroy();
+    Entity() = default;
 
-	explicit operator bool() const;
+    static Entity create();
+    void destroy();
 
-	template<class T> T* getComponent()
-	{
-		if (!bool(*this) || !hasComponent<T>())
-			return nullptr;
-		return &ComponentStorage<T>::storage.sparseset.get(index);
-	}
-	template<class T> const T* getComponent() const
-	{
-		if (!bool(*this) || !hasComponent<T>())
-			return nullptr;
-		return &ComponentStorage<T>::storage.sparseset.get(index);
-	}
-	template<class T> T& addComponent()
-	{
-		ComponentStorage<T>::storage.sparseset.set(index, {});
-		return ComponentStorage<T>::storage.sparseset.get(index);
-	}
-	template<class T, class... ARGS> T& addComponent(ARGS&&... args)
-	{
-		ComponentStorage<T>::storage.sparseset.set(index, T{std::forward<ARGS>(args)...});
-		return ComponentStorage<T>::storage.sparseset.get(index);
-	}
-	template<class T> T& getOrAddComponent()
-	{
-		if (!hasComponent<T>())
-			ComponentStorage<T>::storage.sparseset.set(index, {});
-		return ComponentStorage<T>::storage.sparseset.get(index);
-	}
-	template<class T> bool hasComponent() const
-	{
-		return ComponentStorage<T>::storage.sparseset.has(index);
-	}
-	template<class T> void removeComponent()
-	{
-		ComponentStorage<T>::storage.sparseset.remove(index);
-	}
+    explicit operator bool() const;
 
-	bool operator==(const Entity& other) const;
-	bool operator!=(const Entity& other) const;
+    template<class T> T* getComponent()
+    {
+        if (!bool(*this) || !hasComponent<T>()) return nullptr;
+        return &ComponentStorage<T>::storage.sparseset.get(index);
+    }
+    template<class T> const T* getComponent() const
+    {
+        if (!bool(*this) || !hasComponent<T>()) return nullptr;
+        return &ComponentStorage<T>::storage.sparseset.get(index);
+    }
+    template<class T> T& addComponent()
+    {
+        ComponentStorage<T>::storage.sparseset.set(index, {});
+        return ComponentStorage<T>::storage.sparseset.get(index);
+    }
+    template<class T, class... ARGS> T& addComponent(ARGS&&... args)
+    {
+        ComponentStorage<T>::storage.sparseset.set(index, T{std::forward<ARGS>(args)...});
+        return ComponentStorage<T>::storage.sparseset.get(index);
+    }
+    template<class T> T& getOrAddComponent()
+    {
+        if (!hasComponent<T>())
+            ComponentStorage<T>::storage.sparseset.set(index, {});
+        return ComponentStorage<T>::storage.sparseset.get(index);
+    }
+    template<class T> bool hasComponent() const
+    {
+        return ComponentStorage<T>::storage.sparseset.has(index);
+    }
+    template<class T> void removeComponent()
+    {
+        ComponentStorage<T>::storage.sparseset.remove(index);
+    }
 
-	uint32_t getIndex() const { return index; } // You should never need this, but the multiplayer code does need it.
-	uint32_t getVersion() const { return version; } // You should never need this, but the multiplayer code does need it.
-	static Entity forced(uint32_t index, uint32_t version) { auto e = Entity(); e.index = index; e.version = version; return e; }
+    bool operator==(const Entity& other) const;
+    bool operator!=(const Entity& other) const;
 
-	string toString() const;
-	static Entity fromString(const string& s);
+    // You should never need this, but the multiplayer code does.
+    uint32_t getIndex() const { return index; }
+    // You should never need this, but the multiplayer code does.
+    uint32_t getVersion() const { return version; }
 
-	static void destroyAllEntities();
+    static Entity forced(uint32_t index, uint32_t version)
+    {
+        auto e = Entity();
+        e.index = index;
+        e.version = version;
+        return e;
+    }
 
-	static size_t getActiveCount();
+    string toString() const;
+    static Entity fromString(const string& s);
 
-	static void setPreDestroyCallback(std::function<void(Entity)> callback) {
-		pre_destroy_callback = callback;
-	}
+    static void destroyAllEntities();
 
-	static void dumpDebugInfo();
+    static size_t getActiveCount();
 
-	static constexpr uint32_t no_index = std::numeric_limits<uint32_t>::max();
+    static void setPreDestroyCallback(std::function<void(Entity)> callback)
+    {
+        pre_destroy_callback = callback;
+    }
+
+    static void dumpDebugInfo();
+
+    static constexpr uint32_t no_index = std::numeric_limits<uint32_t>::max();
 private:
-	static constexpr uint32_t destroyed_flag = 1 << (std::numeric_limits<uint32_t>::digits - 1);
+    static constexpr uint32_t destroyed_flag = 1 << (std::numeric_limits<uint32_t>::digits - 1);
 
-	uint32_t index = no_index;
-	uint32_t version = no_index;
+    uint32_t index = no_index;
+    uint32_t version = no_index;
 
-	static Entity fromIndex(uint32_t index);
-	static std::vector<uint32_t> entity_version;
-	static std::vector<uint32_t> free_list;
-	static std::function<void(Entity)> pre_destroy_callback;
+    static Entity fromIndex(uint32_t index);
+    static std::vector<uint32_t> entity_version;
+    static std::vector<uint32_t> free_list;
+    static std::function<void(Entity)> pre_destroy_callback;
 
-	template<class, class...> friend class Query;
+    template<class, class...> friend class Query;
 
-	friend class ::MultiplayerObject;	// We need to be a friend for network replication.
-	friend class ::GameServer;	// We need to be a friend for network replication.
-	friend class ::GameClient;	// We need to be a friend for network replication.
+    // These need to be friends for network replication.
+    friend class ::MultiplayerObject;
+    friend class ::GameServer;
+    friend class ::GameClient;
 };
-
 }
 
-namespace std {
-template <> struct hash<sp::ecs::Entity> {
-	size_t operator()(const sp::ecs::Entity& e) const {
-		return hash<uint32_t>{}(e.getIndex()) + hash<uint32_t>{}(e.getVersion());
-	}
+namespace std
+{
+template <> struct hash<sp::ecs::Entity>
+{
+    size_t operator()(const sp::ecs::Entity& e) const
+    {
+        return hash<uint32_t>{}(e.getIndex()) + hash<uint32_t>{}(e.getVersion());
+    }
 };
 }
