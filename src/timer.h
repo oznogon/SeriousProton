1d5056f3a5990c36f0e1d57fd7fb3a32ecf35195
Oznogon <garrett+ozgh@oznogon.com>
2026-08-08 10:49:04 -0700

    [fork] pragma once, docs for timer.h

--- src/timer.h
+++ src/timer.h
@@ -1,10 +1,9 @@
-#ifndef SP2_TIMER_H
-#define SP2_TIMER_H
+#pragma once
 
 #include <chrono>
 
-namespace sp {
-
+namespace sp
+{
 template<typename ClockSource> class TimerBase
 {
 public:
@@ -34,24 +33,26 @@ public:
         return running;
     }
 
-    //Call this to check if the timer has expired. This will return true only once if the timer expired.
+    // Call this to check if the timer has expired. This returns true only once
+    // if the timer expired.
     bool isExpired()
     {
-        if (!running)
-            return false;
+        if (!running) return false;
+
         bool expired = getTimeElapsed() > timeout;
         if (expired)
         {
             running = auto_restart;
             start_time += std::chrono::duration_cast<typename ClockSource::duration>(std::chrono::duration<float>(timeout));
         }
+
         return expired;
     }
 
     void setProgress(float progress)
     {
-        if (!running)
-            return;
+        if (!running) return;
+
         start_time = ClockSource::now() - std::chrono::duration_cast<typename ClockSource::duration>(std::chrono::duration<float>(timeout * progress));
     }
 
@@ -65,7 +66,8 @@ public:
         return std::chrono::duration<float>(ClockSource::now() - start_time).count();
     }
 
-    // Return the timer progress in the range 0.0 to 1.0, where 0.0 is at the start.
+    // Return the timer progress in the range 0.0 to 1.0, where 0.0 is at the
+    // start.
     float getProgress() const
     {
         return getTimeElapsed() / timeout;
@@ -113,14 +115,14 @@ public:
     static time_point now();
 };
 
-// The normal timer runs at the game speed. If the game is paused or slowed down, this timer slows down as well.
+// The normal timer runs at the game speed. If the game is paused or slowed
+// down, this timer slows down as well.
 using Timer = TimerBase<EngineTime>;
 using Stopwatch = StopwatchBase<EngineTime>;
 
-// The system timer always runs, even if the game is paused or at a different game speed.
+// The system timer always runs, even if the game is paused or at a different
+// game speed.
 using SystemTimer = TimerBase<std::chrono::steady_clock>;
 using SystemStopwatch = StopwatchBase<std::chrono::steady_clock>;
 
 } //!namespace
-
-#endif//SP2_TIMER_H
